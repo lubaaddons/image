b384b7aa9e354a8aadc959a53f1a32c030a112bf
Florian Steiner <fs@adiwidjaja.com>
2016-07-18 14:12:26 +0200

    Initial commit

---
+++ src/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Luba;
+
+use abeautifulsite\SimpleImage;
+
+/**
+ * @see abeautifulsite\SimpleImage
+ * 
+ */
+class Image
+{
+	/**
+	 * Work on an existing image
+	 *
+	 * @var string filename
+	 * @return SimpleImage
+	 */
+	public static function make($filename)
+	{
+		return new SimpleImage($filename);
+	}
+
+	/**
+	 * Create a brand new Image
+	 *
+	 * @param int width
+	 * @param int $height
+	 * @param hex string $color
+	 */
+	public function new($width = NULL, $height = NULL, $color = NULL)
+	{
+		return new SimpleImage(NULL, $width, $height, $color);
+	}
+}
